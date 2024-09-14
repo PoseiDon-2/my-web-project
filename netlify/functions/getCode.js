0e1a02abed13e68be279f756d4316ee2656e1205
PoseiDon <suriya.b@kkumail.com>
2024-09-14 16:56:14 +0700

    V4

--- netlify/functions/getCode.js
+++ netlify/functions/getCode.js
@@ -1,29 +1,46 @@
 const { MongoClient } = require('mongodb');
 
-// เชื่อมต่อกับ MongoDB
-const uri = process.env.MONGODB_URI || 'mongodb+srv://suriyabarisi:<db_password>@cluster0.yhgme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // ใช้ environment variable สำหรับ MongoDB URI
+const uri = process.env.MONGODB_URI || 'mongodb+srv://suriyabarisi:<db_password>@cluster0.yhgme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri);
 
 exports.handler = async function(event, context) {
     if (event.httpMethod === 'GET') {
         const studentId = event.queryStringParameters.studentId;
+        const type = event.queryStringParameters.type; // type can be 'individual' or 'group'
+
+        console.log('Student ID:', studentId);
+        console.log('Type:', type);
 
         try {
+            console.log('Connecting to MongoDB...');
             await client.connect();
-            const database = client.db('assigment_db'); // แทนที่ด้วยชื่อฐานข้อมูลของคุณ
-            const collection = database.collection('assignment_codes'); // แทนที่ด้วยชื่อ collection ของคุณ
+            console.log('Connected to MongoDB');
+
+            const database = client.db('assignment_db');
+            const collection = database.collection('assignment_codes');
 
             const result = await collection.findOne({ studentId: studentId });
+            console.log('Query Result:', result);
+
             await client.close();
 
             if (result) {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        individual_code: result.individual_code,
-                        group_code: result.group_code
-                    }),
-                };
+                if (type === 'individual') {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify({ code: result.individual_code }),
+                    };
+                } else if (type === 'group') {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify({ code: result.group_code }),
+                    };
+                } else {
+                    return {
+                        statusCode: 400,
+                        body: JSON.stringify({ message: 'Invalid type parameter' }),
+                    };
+                }
             } else {
                 return {
                     statusCode: 404,
@@ -31,6 +48,7 @@ exports.handler = async function(event, context) {
                 };
             }
         } catch (error) {
+            console.error('Error:', error);
             return {
                 statusCode: 500,
                 body: JSON.stringify({ message: 'Internal Server Error', error: error.message }),
