29c2e26de2bfb7cc73df1c773a535e38ba47573e
PoseiDon <suriya.b@kkumail.com>
2024-09-14 18:37:14 +0700

    V10

--- netlify/functions/getCode.js
+++ netlify/functions/getCode.js
@@ -1,12 +1,18 @@
 const { MongoClient } = require('mongodb');
 
+// ใช้ Environment Variable สำหรับ MongoDB URI
 const uri = process.env.MONGODB_URI || 'mongodb+srv://your_username:your_password@cluster0.your_cluster.mongodb.net/your_database?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 10000,  // 10 seconds
+    socketTimeoutMS: 30000    // 30 seconds
+});
 
 exports.handler = async function(event, context) {
     if (event.httpMethod === 'GET') {
         const studentId = event.queryStringParameters.studentId;
-        const type = event.queryStringParameters.type;
+        const type = event.queryStringParameters.type; // type can be 'individual' or 'group'
 
         console.log('Student ID:', studentId);
 
@@ -15,8 +21,8 @@ exports.handler = async function(event, context) {
             await client.connect();
             console.log('Connected to MongoDB');
 
-            const database = client.db('assigment_db'); // ชื่อฐานข้อมูล
-            const collection = database.collection('assignment_codes'); // ชื่อ collection
+            const database = client.db('assigment_db'); // แทนที่ด้วยชื่อฐานข้อมูลของคุณ
+            const collection = database.collection('assignment_codes'); // แทนที่ด้วยชื่อ collection ของคุณ
 
             const result = await collection.findOne({ studentId: studentId });
             console.log('Query Result:', result);
